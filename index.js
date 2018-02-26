e6904b280d3e7121a8371908a176224b0448594d
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-25 17:29:28 -0700

    The Reducer Function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
 function createStore () {
   // The store should have four parts
   // 1. The state
