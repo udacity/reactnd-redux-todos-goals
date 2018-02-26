0a5cbcfc7640c231755296108365f7d2aac95f8e
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-25 17:19:55 -0700

    Listening to Changes

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ function createStore () {
   // 4. Update the state
 
   let state
+  let listeners = []
 
   const getState = () => state
 
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
   return {
-    getState
+    getState,
+    subscribe
   }
 }
