d25ff0208351d187b909488661844730b5b78c63
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-25 18:58:46 -0700

    Action Creators

--- index.js
+++ index.js
@@ -37,6 +37,41 @@ const TOGGLE_TODO = 'TOGGLE_TODO'
 const ADD_GOAL = 'ADD_GOAL'
 const REMOVE_GOAL = 'REMOVE_GOAL'
 
+function addTodoAction (todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  }
+}
+
+function removeTodoAction (id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  }
+}
+
+function toggleTodoAction (id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  }
+}
+
+function addGoalAction (goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+function removeGoalAction (id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
+}
+
 function todos (state = [], action) {
   switch(action.type) {
     case ADD_TODO :
@@ -75,60 +110,36 @@ store.subscribe(() => {
   console.log('The new state is: ', store.getState())
 })
 
-store.dispatch({
-  type: ADD_TODO,
-  todo: {
-    id: 0,
-    name: 'Walk the dog',
-    complete: false,
-  }
-})
+store.dispatch(addTodoAction({
+  id: 0,
+  name: 'Walk the dog',
+  complete: false,
+}))
 
-store.dispatch({
-  type: ADD_TODO,
-  todo: {
-    id: 1,
-    name: 'Wash the car',
-    complete: false,
-  }
-})
+store.dispatch(addTodoAction({
+  id: 1,
+  name: 'Wash the car',
+  complete: false,
+}))
 
-store.dispatch({
-  type: ADD_TODO,
-  todo: {
-    id: 2,
-    name: 'Go to the gym',
-    complete: true,
-  }
-})
+store.dispatch(addTodoAction({
+  id: 2,
+  name: 'Go to the gym',
+  complete: true,
+}))
 
-store.dispatch({
-  type: REMOVE_TODO,
-  id: 1
-})
+store.dispatch(removeTodoAction(1))
 
-store.dispatch({
-  type: TOGGLE_TODO,
-  id: 0
-})
+store.dispatch(toggleTodoAction(0))
 
-store.dispatch({
-  type: ADD_GOAL,
-  goal: {
-    id: 0,
-    name: 'Learn Redux'
-  }
-})
+store.dispatch(addGoalAction({
+  id: 0,
+  name: 'Learn Redux'
+}))
 
-store.dispatch({
-  type: ADD_GOAL,
-  goal: {
-    id: 1,
-    name: 'Lose 20 pounds'
-  }
-})
+store.dispatch(addGoalAction({
+  id: 1,
+  name: 'Lose 20 pounds'
+}))
 
-store.dispatch({
-  type: REMOVE_GOAL,
-  id: 0
-})
+store.dispatch(removeGoalAction(0))
