e44e5c9907a04e4cd94440c11dcbf91b04ee24c9
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-25 18:13:52 -0700

    Goals Reducer

--- index.js
+++ index.js
@@ -45,6 +45,17 @@ function todos (state = [], action) {
   }
 }
 
+function goals (state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+    default :
+      return state
+  }
+}
+
 const store = createStore(todos)
 
 store.subscribe(() => {
