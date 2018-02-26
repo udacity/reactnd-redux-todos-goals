bccfab8ce76658091d431a30113b47ff13acebfc
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-25 18:05:01 -0700

    More Actions

--- index.js
+++ index.js
@@ -32,11 +32,17 @@ function createStore (reducer) {
 
 // App Code
 function todos (state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
+  switch(action.type) {
+    case 'ADD_TODO' :
+      return state.concat([action.todo])
+    case 'REMOVE_TODO' :
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO' :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }))
+    default :
+      return state
   }
-
-  return state
 }
 
 const store = createStore(todos)
