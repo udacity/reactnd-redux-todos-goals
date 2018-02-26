7624575a8e2b41f71c66f9b5ff7ff701077167c0
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-25 17:48:45 -0700

    Create Store: Dispatch

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-function todos (state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
-  }
-
-  return state
-}
-
-function createStore () {
+// Library Code
+function createStore (reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
@@ -25,8 +18,23 @@ function createStore () {
     }
   }
 
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch,
   }
+}
+
+// App Code
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
 }
